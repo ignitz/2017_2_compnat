2ed9ecac6125be27aee3f00acc1bb6f643078708
ignitz <ignitzhjfk@gmail.com>
2017-09-24 18:49:57 -0300

    open CSV file

--- tp1/src/main.cpp
+++ tp1/src/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include "csv/csv.hpp"
+#include "ga/ga.hpp"
 
-int main(int argc, char const *argv[])
+void test_csv(std::string dataset) {
+  Csv csv (dataset);
+  if (csv.open_file())
+  {
+    csv.read();
+    csv.close();
+  }
+  else {
+    std::cout << "Deu tudo errado" << std::endl;
+  }
+  
+};
+
+// int argc, char const *argv[]
+int main()
 {
-	std::cout << "Teste" << std::endl;
-	return 0;
+  test_csv("datasets/keijzer-10-train.csv");
+
+  // test_csv("datasets/keijzer-10-test.csv");
+  // test_csv("datasets/house-train.csv");
+  // test_csv("datasets/house-test.csv");
+  // test_csv("datasets/keijzer-7-train.csv");
+  // test_csv("datasets/keijzer-7-test.csv");
+  return 0;
 }
