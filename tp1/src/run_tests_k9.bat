54fd8b6a4c131819232e29110f0364d4d12c37e3
ignitz <ignitzhjfk@gmail.com>
2017-10-02 21:46:12 -0300

    tp1 done... i thing

--- tp1/src/run_tests_k9.bat
+++ tp1/src/run_tests_k9.bat
@@ -28,12 +28,3 @@ python gp.py datasets/keijzer-10-train.csv datasets/keijzer-10-test.csv output/k
 python gp.py datasets/keijzer-10-train.csv datasets/keijzer-10-test.csv output/keijzer-10/28.txt 100 500 0.51 0.74 7 3
 python gp.py datasets/keijzer-10-train.csv datasets/keijzer-10-test.csv output/keijzer-10/29.txt 100 500 0.51 0.74 7 3
 python gp.py datasets/keijzer-10-train.csv datasets/keijzer-10-test.csv output/keijzer-10/30.txt 100 500 0.42 0.9 7 3
-python gp.py datasets/house-train.csv datasets/house-test.csv output/house/1.txt 50 50 0.9 0.05 2 2
-python gp.py datasets/house-train.csv datasets/house-test.csv output/house/2.txt 50 50 0.9 0.05 2 2
-python gp.py datasets/house-train.csv datasets/house-test.csv output/house/3.txt 50 50 0.67 0.78 5 2
-python gp.py datasets/house-train.csv datasets/house-test.csv output/house/4.txt 100 50 0.67 0.78 5 2
-python gp.py datasets/house-train.csv datasets/house-test.csv output/house/5.txt 100 50 0.67 0.78 5 2
-python gp.py datasets/house-train.csv datasets/house-test.csv output/house/6.txt 100 50 0.93 0.18 5 2
-python gp.py datasets/house-train.csv datasets/house-test.csv output/house/7.txt 500 50 0.93 0.18 5 2
-python gp.py datasets/house-train.csv datasets/house-test.csv output/house/8.txt 500 50 0.93 0.18 5 2
-python gp.py datasets/house-train.csv datasets/house-test.csv output/house/9.txt 500 50 0.08 0.22 5 2
